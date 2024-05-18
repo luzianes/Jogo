f97040e6a32ef1650526971aa7238a236946ebc1
luzianes <luzianes@gmail.com>
2024-05-18 20:31:01 -0300

    Adicionando #include <stdio.h>

--- main.c
+++ main.c
@@ -1,12 +1,5 @@
-/**
- * main.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
-
+#include <studio.h>
 #include <string.h>
-
 #include "screen.h"
 #include "keyboard.h"
 #include "timer.h"
