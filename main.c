8e4b03a81043c6ca3756203e29f453aeefccfcf5
luzianes <luzianes@gmail.com>
2024-05-19 20:00:43 -0300

    Construção da raquete (função iniciar e imprimir).

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "screen.h"
 #include "keyboard.h"
 #include "timer.h"
 
+typedef struct Raquete {
+    int x, y;     // posição (x, y) da raquete
+    int altura;   // altura da raquete
+    char simbolo;  // símbolo
+} raquete;
+
 int x = 34, y = 12;
 int incX = 1, incY = 1;
+int qtde_raquete = 2;
 
-void printO(int nextX, int nextY)
+void printO(int nextX, int nextY) //Letra "O"
 {
     screenSetColor(CYAN, DARKGRAY);
     screenGotoxy(x, y);
@@ -18,17 +26,36 @@ void printO(int nextX, int nextY)
     printf("O");
 }
 
+void iniciar_raquete(raquete *rptr, int x, int y, int altura, char simbolo) {
+    rptr->x = x;
+    rptr->y = y;
+    rptr->altura = altura;
+    rptr->simbolo = simbolo;
+}
 
+void imprimir_raquete(raquete *rptr) {
+    screenSetColor(CYAN, DARKGRAY);
+    for (int i = 0; i < raquete->altura; i++) {
+        screenGotoxy(raquete->x, raquete->y + i);
+        printf("%c", raquete->simbolo);
+    }
+}
 
 int main()
 {
     static int ch = 0;
+
+    raquete *rptr = (raquete*)malloc(qtde_raquete * sizeof(raquete));
 
     screenInit(1);
     keyboardInit();
     timerInit(50);
 
     printO(x, y);
+
+    iniciar_raquete(&rptr[0], 2, 10, 4, '|');  // Raquete esquerda
+    iniciar_raquete(&rptr[1], 77, 10, 4, '|'); // Raquete direita
+
     screenUpdate();
 
     while (ch != 27) //Jogo ativo enquanto não teclar ESC
@@ -49,12 +76,16 @@ int main()
             int newY = y + incY;
             if (newY >= MAXY-1 || newY <= MINY+1) incY = -incY;
 
+            for (int i = 0; i < qtde_raquete; i++) {
+                imprimir_raquete(&rptr[i]);
+            }
 
             printO(newX, newY);
 
             screenUpdate();
         }
     }
+    free (rptr);
 
     keyboardDestroy();
     screenDestroy();
