6932c6cf1f1cfb91e9a6756381b0188fdd767e12
luzianes <luzianes@gmail.com>
2024-05-19 20:34:37 -0300

    Correção - mudança nos parâmetros da raquete

--- main.c
+++ main.c
@@ -58,8 +58,8 @@ int main()
     printO(x, y);
 
     //Inicia as raquetes com a posição (x,y), largura, altura e símbolo
-    iniciar_raquete(&rptr[0], 4, 10, 10, 10, '|');  // Raquete esquerda
-    iniciar_raquete(&rptr[1], 77, 10, 10, 10, '|'); // Raquete direita
+    iniciar_raquete(&rptr[0], 3, 10, 10, 5, '|');  // Raquete esquerda
+    iniciar_raquete(&rptr[1], 77, 10, 10, 5, '|'); // Raquete direita
 
     screenUpdate();
 
